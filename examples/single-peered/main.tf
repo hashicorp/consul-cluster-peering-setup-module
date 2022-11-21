6ecaa4cfd43376a119b5294c4876a0aa17f57b5b
Michael Wilkerson <mwilkerson@hashicorp.com>
2022-11-21 14:32:04 -0800

    added a single peering example

---
+++ examples/single-peered/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  # This is local, because it's simply generating from a template and not changing consul.
+  # The output files from this module are what should be used with terraform cloud / remote.
+  backend "local" {}
+}
+
+# This will generate a terraform module on disk. The module creates
+# peerings that are the cross-product of all acceptors to all dialers.
+# In this examples one cluster is peered with three other clusters/partitions
+module "my_peerings" {
+  source            = "../.."
+  peering_acceptors = [
+    { alias: "cluster1" },
+  ]
+  peering_dialers = [
+    { alias: "cluster2" },
+    { alias: "cluster2", partition: "mypart" },
+    { alias: "cluster3" },
+  ]
+}
+
+
